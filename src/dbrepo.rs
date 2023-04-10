4b497dd169ff7890af3597f94bfbe30d63f15992
Lewis Torrington <lewis.torrington@upcloud.com>
2023-04-10 11:25:06 +0300

    chore: removed commented BS

--- src/dbrepo.rs
+++ src/dbrepo.rs
@@ -7,17 +7,6 @@ mod models;
 
 use models::FarmDetail;
 
-// use chrono::{NaiveDateTime, ParseError};
-// use sqlx::{Decode, Error, MySql};
-
-// impl<'r> Decode<'r, MySql> for NaiveDateTime {
-//     fn decode(value: sqlx::mysql::MySqlValueRef<'r>) -> Result<Self, Error> {
-//         let s = <&str as Decode<MySql>>::decode(value)?;
-//         NaiveDateTime::parse_from_str(s, "%Y-%m-%d %H:%M:%S")
-//             .map_err(|err: ParseError| Error::Decode(Box::new(err)))
-//     }
-// }
-
 pub async fn get_farm_details(mut db: Connection<DBRepo>) -> Result<Vec<FarmDetail>, sqlx::Error> {
     let select_query = sqlx::query(
         "SELECT id, name, description, joined_date, address, display_address, primary_produce, website FROM farm_details"
